37cb4e4fe8bae6b3f329146b45a527ff315b3b5b
DanielCorralesAlonso <danielcorralesalonso@gmail.com>
2024-05-15 20:10:36 +0200

    correction in resnet own

--- src/model/resnet_own.py
+++ src/model/resnet_own.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-class ResNetUSCL(nn.Module):
+class ResNetOwn(nn.Module):
     def __init__(self, hidden_dim=256, last_dim=4, freeze=True, discard_layers=2, dropout=0.1, pretrained='', weights_path='', device='cpu', **kwargs):
-        super(ResNetUSCL, self).__init__()
+        super(ResNetOwn, self).__init__()
 
-        self.resnet = models.resnet18(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features  
+        # self.resnet = models.resnet18(pretrained=pretrained)
+        # self.resnet = models.resnet50(pretrained = pretrained)
+        self.resnet = models.vit_b_16(pretrained=pretrained)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, last_dim)   
 
         self.device = torch.device(device)
